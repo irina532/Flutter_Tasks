1eb266289de03eb813ac9fa0be667f325eb30f5a
erina akter <erinaakter2001@gmail.com>
2024-12-14 17:09:24 +0600

    somehow done the tasks

--- lib/Modules/Home/Home_view.dart
+++ lib/Modules/Home/Home_view.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:tasks/Modules/Task_1/Task_1_view.dart';
 import 'package:tasks/Modules/Task_2/Task_2_view.dart';
 import 'package:tasks/Modules/Task_3/Task_3_view.dart';
+import 'package:tasks/Modules/Tasks/TaskButton/button_task.dart';
+import 'package:tasks/Modules/Tasks/grid_view/grid_view.dart';
+import 'package:tasks/Modules/Tasks/shoes/shoes.dart';
 
 class HomeScreen extends StatelessWidget{
   final List<Map<String,dynamic>>pages =[
     {'title':'Task_1','page':Task_1()},
     {'title':'Task_2','page':Task_2()},
     {'title':'Task_3','page':Task_3()},
+    {'title':'Shoes','page':Shoes()},
+    {'title':'Grid','page':GridViewExample()},
+    {'title':'Button','page':ButtonTaskExample()},
     
     
   ];
--- lib/Modules/Task_2/Task_2_view.dart
+++ lib/Modules/Task_2/Task_2_view.dart
@@ -6,6 +6,23 @@ class Task_2 extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Card Widget Including Circle avator, Text Widget Description and Elevated Button'),),
+      body: Center(
+        child: Card(
+          
+          child: Column(
+            children: [
+              CircleAvatar(
+            radius: 50,
+            backgroundColor: Colors.brown,
+            backgroundImage: AssetImage("assets/images/shinchain-2.jpg"),
+          ),
+          
+            ] 
+            
+          ),
+          
+        ),
+      ),
       
     );
   }
